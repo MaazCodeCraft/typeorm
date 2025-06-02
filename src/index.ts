dcf5b45cdb274b352559e22bdc4546c6484839ca
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-03 00:46:17 +0500

    Connection with database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello');
+import { createConnection } from "typeorm";
+
+const main = async () => {
+    try {
+        const connection = await createConnection ({
+        type: "mariadb",
+        host: "127.0.0.1",
+        port: 3306,
+        username: "root",
+        password: "Pak@1947",
+        database: "typeorm"
+    })
+    console.log('Successfully Connected');
+    } catch (error) {
+        console.log(error);
+        throw new Error("Unable to connect to database");
+    }
+}
+
+main();
