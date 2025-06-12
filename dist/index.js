45165e161ff0cf39e1b2c9d4b95b874828a3f314
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-12 23:25:39 +0500

    Create connection to database

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log('hi');
+Object.defineProperty(exports, "__esModule", { value: true });
+const typeorm_1 = require("typeorm");
+const main = async () => {
+    try {
+        const connection = await (0, typeorm_1.createConnection)({
+            type: "mariadb",
+            host: "127.0.0.1",
+            port: 3306,
+            username: "root",
+            password: "Pak@1947",
+            database: "typeorm",
+            synchronize: true
+        });
+        console.log("Successful Connected");
+    }
+    catch (err) {
+        console.log(err);
+        throw new Error('Unable to connect to database');
+    }
+};
+main();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hi!!!');
+import { createConnection } from "typeorm";
+
+const main = async () => {
+    try {
+        const connection = await createConnection ({
+        type: "mariadb",
+        host: "127.0.0.1",
+        port: 3306,
+        username: "root",
+        password: "Pak@1947",
+        database: "typeorm",
+        synchronize: true
+    });
+        console.log("Successful Connected");
+    } catch (err) {
+        console.log(err);
+        throw new Error('Unable to connect to database');
+    }
+    
+}
+
+main ();
