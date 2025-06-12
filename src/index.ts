ad8229a02b43663d2fe0f1e95beb86e46c4e733a
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-13 00:25:27 +0500

    Creating and inserting a photo into the database

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import { AppDataSource } from "./data-source";
+import { Photo } from "./entities/user";
 
-AppDataSource.initialize()
-    .then (() => {
-        console.log("Data Source has been initialized!");
-    })
-    .catch ((err) => {
-        console.error("Error during Data Source initialization:", err);
-    })
+async function main() {
+    await AppDataSource.initialize()
+        .then (() => {
+            console.log("Data Source has been initialized!");
+        })
+        .catch ((err) => {
+            console.error("Error during Data Source initialization:", err);
+        })
+    
+    const photo = new Photo ();
+        photo.name = "Me and Bears";
+        photo.description = "I am near polar bears";
+        photo.filename = "photo-with-bears.jpg";
+        photo.view = 10;
+        photo.isPublished = true;
+    
+    await AppDataSource.manager.save(photo);
+    console.log("Photo has been saved. Photo id is, ", photo.id);
+}
+
+main().catch(err => console.error(err));
+    
