bbc5ce1ff97bbdb570517d2fb5a61a85a386f555
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-17 22:32:38 +0500

    Loading from the database

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,9 @@ async function main() {
         .catch ((err) => {
             console.error("Error during Data Source initialization:", err);
         })
-    
-    const photo = new Photo ();
+        
+        /*
+        const photo = new Photo ();
         photo.name = "Me and Bears";
         photo.description = "I am near polar bears";
         photo.filename = "photo-with-bears.jpg";
@@ -22,6 +23,31 @@ async function main() {
         console.log("Photo has been saved");
         const savedPhotos = await photoRepository.find();   
         console.log("All photos from the db: ", savedPhotos);
+        */
+
+        const photoRepository = AppDataSource.getRepository(Photo);
+        const allPhotos = await photoRepository.find();
+        console.log("All photos from the db: ", allPhotos);
+
+        const firstPhoto = await photoRepository.findOneBy({
+            id: 1,
+        });
+        console.log("First photo from the db: ", firstPhoto);
+
+        const meAndBearsPhoto = await photoRepository.findOneBy({
+            name: "Me and Bears",
+        });
+        console.log("Me and Bears photo from the db: ", meAndBearsPhoto);
+
+        const allViewedPhotos = await photoRepository.findBy({ view: 1 });
+        console.log("All viewed photos: ", allViewedPhotos);
+
+        const allPublishedPhotos = await photoRepository.findBy({ isPublished: true });
+        console.log("All published photos: ", allPublishedPhotos);
+
+        const [photos, photosCount] = await photoRepository.findAndCount();
+        console.log("All photos: ", photos);
+        console.log("Photos count: ", photosCount);
 }
 
 main().catch(err => console.error(err));
